3b5177cd0e1c6fa8c4a77f09fc1b51a2eb7c3591
sharanya <bsharanya25@gmail.com>
2024-03-18 11:18:40 +0530

    New commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { drawBinaryTree } from './drawBinaryTree.js'
+import { BinaryTreeNode } from './BinaryTreeNode.js';
+
+function App() {
+  return (
+    <div className="App">
+      <drawBinaryTree />
+    </div >
+
+
+  );
+}
+
+export default App;
